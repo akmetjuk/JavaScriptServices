e3a68d9da293173cb8422dca8cffccbdaf8ab299
Andrii Kmetiuk <akmetjuk@gmail.com>
2019-01-15 22:03:04 +0200

    Add project files.

---
+++ JavaScriptServices/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.NodeServices;
+
+namespace JavaScriptServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly INodeServices _nodeServices;
+
+        public ValuesController(INodeServices nodeServices)
+        {
+            _nodeServices = nodeServices;
+        }
+
+
+        // GET api/values
+        [HttpGet]
+        //public ActionResult<IEnumerable<string>> Get()
+        public async Task<string> Get()
+        {
+            // return new string[] { "value1", "value2" };
+            return await _nodeServices.InvokeAsync<string>("./scripts/js/greeter", "Joseph", "Woodward");
+
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ JavaScriptServices/scripts/js/greeter.js
@@ -0,0 +1,9 @@
+﻿// greeter.js
+module.exports = function (callback, firstName, surname) {
+
+    var greet = function (firstName, surname) {
+        return "Hello " + firstName + " " + surname;
+    }
+
+    callback(null, greet(firstName, surname));
+}
